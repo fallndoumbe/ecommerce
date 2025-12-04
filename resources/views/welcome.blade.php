0660de6785ab0003bcbf0d743453d0c34290f086
fallndoumbe <ndoumbefall163@gmail.com>
2025-12-04 17:11:53 +0000

    Ton message de modification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,17 @@
             </style>
         @endif
     </head>
+    <div class="container">
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        
+    <div class="container">
+        <h1>Test des boutons</h1>
+        <p>Clique sur un bouton pour tester :</p>
+
+        <button class="btn-ajouter" onclick="alert('Ajouter !')">Ajouter</button>
+        <button class="btn-modifier" onclick="alert('Modifier !')">Modifier</button>
+        <button class="btn-supprimer" onclick="alert('Supprimer !')">Supprimer</button>
+    </div>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
